309b95232c1e571e60983468084b8de6109b8aa9
ctf0 <muah003@gmail.com>
2019-12-23 00:45:01 +0200

    v1
    good but slow

--- extension.js
+++ extension.js
@@ -1,3 +1,4 @@
+const { EOL } = require('os')
 const vscode = require('vscode')
 
 /**
@@ -5,30 +6,26 @@ const vscode = require('vscode')
  */
 function activate(context) {
 
-    // count for indentation
-    // if not then get the doc text from/to current position
-    // and regex replace
-    //
-
     // delete
+    // |>............word
     context.subscriptions.push(
-        vscode.commands.registerCommand('smart-delete.right', () => {
+        vscode.commands.registerCommand('smart-delete.right', async () => {
             let editor = vscode.window.activeTextEditor
             let { document, selections } = editor
 
-            for (const item of selections) {
-                let txt = document.lineAt(item.active.line).text
-                let search = document.getText(
-                    new vscode.Range(
-                        item.active.line,
-                        item.active.character,
-                        item.end.line,
-                        txt.length
+            // work top > bottom
+            for (const item of invertSelections(selections)) {
+                if (await currentLineCheck(document, item, 'right')) {
+                    let range = new vscode.Range(
+                        item.active.line, // cursor line
+                        item.active.character, // cursor position
+                        document.lineCount, // end line of doc
+                        document.positionAt(document.getText().length - 1).character // end char of doc
                     )
-                )
+                    let search = await document.getText(range)
 
-                if (/^\s+/.test(search)) {
-                    vscode.commands.executeCommand('deleteWordRight')
+                    await editor.edit((edit) => edit.replace(range, rightReplace(search)))
+                    await sleep(1000)
                 } else {
                     vscode.commands.executeCommand('deleteRight')
                 }
@@ -37,40 +34,92 @@ function activate(context) {
     )
 
     // backspace
+    // word............<|
     context.subscriptions.push(
-        vscode.commands.registerCommand('smart-delete.left', () => {
+        vscode.commands.registerCommand('smart-delete.left', async () => {
             let editor = vscode.window.activeTextEditor
             let { document, selections } = editor
 
-            for (const item of selections) {
-                let search = document.getText(
-                    new vscode.Range(
-                        item.active.line,
-                        0,
-                        item.end.line,
-                        item.end.character
+            // work bottom > top
+            for (const item of invertSelections(selections)) {
+                if (await currentLineCheck(document, item, 'left')) {
+                    let range = new vscode.Range(
+                        0, // start line of doc
+                        0, // start char of doc
+                        item.end.line, // cursor line
+                        item.end.character // cursor position
                     )
-                )
+                    let search = await document.getText(range)
 
-                if (/\s+$/.test(search)) {
-                    vscode.commands.executeCommand('deleteWordLeft')
+                    await editor.edit((edit) => edit.replace(range, leftReplace(search)))
                 } else {
                     vscode.commands.executeCommand('deleteLeft')
                 }
             }
         })
     )
-
-    // del
-    // txt = txt.replace(/((\s{2,})\S)|\S/m, (match) => {
-    //     return match[match.length - 1]
-    // })
-
-    // backspace
-    // txt = txt.replace(/(?!\S)(\s{2,})/m, (match) => {
-    //     return match[match.length - 1]
-    // })
 }
+
+async function currentLineCheck(document, item, dir) {
+    let active = item.active
+    let end = item.end
+    let isLeft = dir == 'left'
+    let regex = isLeft ? /\s{2,}$/ : /^\s{2,}/
+    let txt = await document.lineAt(active.line).text
+    let search = await document.getText(
+        new vscode.Range(
+            active.line,
+            isLeft ? 0 : active.character,
+            end.line,
+            isLeft ? end.character : txt.length
+        )
+    )
+
+    if (search.length == 0) {
+        // ............word
+        // <|............word
+        if (active.character == 0 && isLeft && txt.match(/^\s+/)) {
+            vscode.commands.executeCommand('deleteWordRight')
+
+            return false
+        }
+
+        // EOL|>
+        // ............word
+        // or
+        // ............word
+        // <|SOF
+        return true
+    }
+
+    return regex.test(search)
+}
+
+function rightReplace(txt) {
+    return txt.replace(/\s{2,}\S/m, (match) => {
+        return match.trim()
+    })
+}
+
+function leftReplace(txt) {
+    return txt.replace(/\S\s{2,}$/g, (match) => {
+        return match.trim()
+    })
+}
+
+function invertSelections(arr) {
+    return arr.sort((a, b) => { // make sure its sorted correctly
+        if (a.start.line > b.start.line) return 1
+        if (b.start.line > a.start.line) return -1
+
+        return 0
+    }).reverse()
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 exports.activate = activate
 
 function deactivate() { }
