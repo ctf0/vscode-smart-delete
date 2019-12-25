32a386806a4d36a7472d141a093507abfed75cd4
ctf0 <muah003@gmail.com>
2019-12-25 02:11:17 +0200

    005
    v1 + v2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,3 +11,9 @@
 ## 0.0.3
 
 - fix cant remove empty consecutive lines
+
+## 0.0.5
+
+- new flow if `keepOneLine: true` "now its even smarter"
+    - if in the current line has text and u press del/backspace, it will remove the empty spaces as expected "no more jumping around"
+    - if the previous line have text, and u press del/backspace, it will ignore the newline rule ex.`}\n) => })` again as expected "no more jumping around"
--- extension.js
+++ extension.js
@@ -26,30 +26,38 @@ function activate(context) {
             // multi line selection or multi cursors
             if (selections.length > 1 || !currentSelection.isSingleLine) {
                 vscode.commands.executeCommand('deleteRight')
-            } else {
-                let range = new vscode.Range(
-                    currentSelection.active.line, // cursor line
-                    currentSelection.active.character, // cursor position
-                    document.lineCount, // doc end line
-                    document.positionAt(document.getText().length - 1).character // doc end char
-                )
-                let search = await document.getText(range)
+            }
 
-                if (/^\s{2,}/.test(search)) { // multiline
-                    if (!search.trim()) { // nothing but empty lines
-                        await editor.edit((edit) => edit.replace(range, replace(search, /\s+/g, 'right')))
-                    } else { // normal
-                        await editor.edit((edit) => edit.replace(range, replace(search, /\s{2,}\S/m, 'right')))
+            // current line
+            if (await currentLineCheck(document, currentSelection, 'right')) {
+                return vscode.commands.executeCommand('deleteWordRight')
+            }
+
+            // |>............direct next line
+            let prevLineHasText = await otherLineCheck(document, currentSelection, 'right')
+
+            let range = new vscode.Range(
+                currentSelection.active.line, // cursor line
+                currentSelection.active.character, // cursor position
+                document.lineCount, // doc end line
+                document.positionAt(document.getText().length - 1).character // doc end char
+            )
+            let search = await document.getText(range)
+
+            if (/^\s{2,}/.test(search)) { // multiline
+                if (!search.trim()) { // nothing but empty lines
+                    await editor.edit((edit) => edit.replace(range, replace(search, /\s+/g, 'right')))
+                } else { // normal
+                    await editor.edit((edit) => edit.replace(range, replace(search, /\s{2,}\S/m, 'right')))
+
+                    if (!prevLineHasText) {
+                        insertNewLine()
                     }
-
-                    insertNewLine()
-                } else if (new RegExp(`^${EOL}`).test(search)) { // end of line
-                    await editor.edit((edit) => edit.replace(range, replace(search, new RegExp(EOL, 'm'), 'right')))
-
-                    insertNewLine()
-                } else {
-                    vscode.commands.executeCommand('deleteRight')
                 }
+            } else if (new RegExp(`^${EOL}`).test(search)) { // end of line
+                await editor.edit((edit) => edit.replace(range, replace(search, new RegExp(EOL, 'm'), 'right')))
+            } else {
+                vscode.commands.executeCommand('deleteRight')
             }
         })
     )
@@ -62,31 +70,85 @@ function activate(context) {
             let { document, selections } = editor
             let currentSelection = selections[0]
 
+            // multi line selection or multi cursors
             if (selections.length > 1 || !currentSelection.isSingleLine) {
-                vscode.commands.executeCommand('deleteLeft')
-            } else {
-                let range = new vscode.Range(
-                    0, // doc start line
-                    0, // doc start char
-                    currentSelection.end.line, // cursor line
-                    currentSelection.end.character // cursor position
-                )
-                let search = await document.getText(range)
+                return vscode.commands.executeCommand('deleteLeft')
+            }
 
-                if (/\s{2,}$/.test(search)) {
-                    if (!search.trim()) { // nothing but empty lines
-                        await editor.edit((edit) => edit.replace(range, replace(search, /\s+/g, 'left')))
-                    } else { // normal
-                        await editor.edit((edit) => edit.replace(range, replace(search, /\S\s{2,}$/g, 'left')))
+            // current line
+            if (await currentLineCheck(document, currentSelection, 'left')) {
+                return vscode.commands.executeCommand('deleteWordLeft')
+            }
+
+            // direct prev line............<|
+            let prevLineHasText = await otherLineCheck(document, currentSelection, 'left')
+
+            let range = new vscode.Range(
+                0, // doc start line
+                0, // doc start char
+                currentSelection.end.line, // cursor line
+                currentSelection.end.character // cursor position
+            )
+            let search = await document.getText(range)
+
+            if (/\s{2,}$/.test(search)) {
+                if (!search.trim()) { // nothing but empty lines
+                    await editor.edit((edit) => edit.replace(range, replace(search, /\s+/g, 'left')))
+                } else { // normal
+                    await editor.edit((edit) => edit.replace(range, replace(search, /\S\s{2,}$/g, 'left')))
+
+                    if (!prevLineHasText) {
+                        insertNewLine()
                     }
-
-                    insertNewLine()
-                } else {
-                    vscode.commands.executeCommand('deleteLeft')
                 }
+            } else {
+                vscode.commands.executeCommand('deleteLeft')
             }
         })
     )
+}
+
+async function currentLineCheck(document, cursor, dir) {
+    if (config.keepOneLine) {
+        let active = cursor.active
+        let end = cursor.end
+        let isLeft = dir == 'left'
+        let regex = isLeft ? /\S\s{2,}/ : /\s{2,}\S/
+        let txt = await document.lineAt(active.line).text
+        let search = await document.getText(
+            new vscode.Range(
+                active.line,
+                isLeft ? 0 : active.character,
+                end.line,
+                isLeft ? end.character : txt.length
+            )
+        )
+
+        return regex.test(search)
+    }
+
+    return false
+}
+
+// prev or next
+async function otherLineCheck(document, cursor, dir) {
+    if (config.keepOneLine) {
+        // check for cursor current location
+        // ............<|word
+        // word|>............
+        if (!await currentLineCheck(document, cursor, dir)) {
+            let active = cursor.active.line
+            let isLeft = dir == 'left'
+            let line = isLeft
+                ? active - 1
+                : active + 1
+            let txt = await document.lineAt(line).text
+
+            return txt.trim()
+        }
+    }
+
+    return false
 }
 
 function replace(txt, regex, dir) {
